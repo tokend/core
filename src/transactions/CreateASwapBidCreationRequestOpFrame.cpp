fba2736e654536db8f6a217df02d7cd6d1d73c69
Yevhenii Luhanskyi <y.luhanskyi@gmail.com>
2018-09-05 18:43:45 +0300

    Add "Create atomic swap bid creation request" operation

---
+++ src/transactions/CreateASwapBidCreationRequestOpFrame.cpp
@@ -0,0 +1,198 @@
+#include "main/Application.h"
+#include "database/Database.h"
+#include <transactions/dex/OfferManager.h>
+#include <ledger/BalanceHelper.h>
+#include <ledger/AssetHelper.h>
+#include <ledger/ReviewableRequestFrame.h>
+#include <ledger/ReviewableRequestHelper.h>
+#include "CreateASwapBidCreationRequestOpFrame.h"
+
+using namespace std;
+
+namespace stellar
+{
+
+CreateASwapBidCreationRequestOpFrame::CreateASwapBidCreationRequestOpFrame(
+        Operation const &op, OperationResult &opRes, TransactionFrame &parentTx)
+        : OperationFrame(op, opRes, parentTx),
+          mCreateASwapBidCreationRequest(
+                  mOperation.body.createASwapBidCreationRequestOp())
+{
+}
+
+std::unordered_map<AccountID, CounterpartyDetails>
+CreateASwapBidCreationRequestOpFrame::getCounterpartyDetails(Database &db,
+                                                          LedgerDelta *delta) const
+{
+    // no counterparties
+    return {};
+}
+
+SourceDetails CreateASwapBidCreationRequestOpFrame::getSourceAccountDetails(
+        std::unordered_map<AccountID, CounterpartyDetails> counterpartiesDetails,
+        int32_t ledgerVersion) const
+{
+    return SourceDetails(getAllAccountTypes(), mSourceAccount->getHighThreshold(),
+                         static_cast<int32_t>(SignerType::ATOMIC_SWAP_MANAGER));
+}
+
+
+bool CreateASwapBidCreationRequestOpFrame::isQuoteAssetValid(Database& db,
+                                                          AssetCode baseAssetCode,
+                                                          AssetCode quoteAssetCode)
+{
+    if (baseAssetCode == quoteAssetCode)
+    {
+        innerResult().code(CreateASwapBidCreationRequestResultCode::ASSETS_ARE_EQUAL);
+        return false;
+    }
+
+    auto quoteAssetFrame = AssetHelper::Instance()->loadAsset(quoteAssetCode, db);
+
+    if (quoteAssetFrame == nullptr)
+    {
+        innerResult().code(
+                CreateASwapBidCreationRequestResultCode::QUOTE_ASSET_NOT_FOUND);
+        return false;
+    }
+
+    if (quoteAssetFrame->isPolicySet(AssetPolicy::CAN_BE_QUOTE_IN_ATOMIC_SWAP))
+    {
+        innerResult().code(
+                CreateASwapBidCreationRequestResultCode::QUOTE_ASSET_CANNOT_BE_SWAPPED);
+        return false;
+    }
+
+    return true;
+}
+
+bool CreateASwapBidCreationRequestOpFrame::areQuoteAssetsValid(Database& db,
+                                                            AssetCode baseAssetCode)
+{
+    auto& quoteAssets = mCreateASwapBidCreationRequest.request.quoteAssets;
+
+    for (auto const& quoteAsset : quoteAssets)
+    {
+        if (!isQuoteAssetValid(db, baseAssetCode, quoteAsset))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool CreateASwapBidCreationRequestOpFrame::doApply(Application &app, LedgerDelta &delta,
+                                                LedgerManager &ledgerManager)
+{
+    Database& db = app.getDatabase();
+
+    auto& const aSwapBidCreationRequest = mCreateASwapBidCreationRequest.request;
+
+    BalanceFrame::pointer baseBalanceFrame =
+            BalanceHelper::Instance()->loadBalance(aSwapBidCreationRequest.baseBalance,
+                                                   db);
+    if (baseBalanceFrame == nullptr)
+    {
+        innerResult().code(
+                CreateASwapBidCreationRequestResultCode::BASE_BALANCE_NOT_FOUND);
+        return false;
+    }
+
+    AssetFrame::pointer baseAssetFrame =
+            AssetHelper::Instance()->loadAsset(baseBalanceFrame->getAsset(), db);
+    if (baseAssetFrame == nullptr)
+    {
+        innerResult().code(
+                CreateASwapBidCreationRequestResultCode::BASE_ASSET_NOT_FOUND);
+        return false;
+    }
+
+    if (baseAssetFrame->isPolicySet(AssetPolicy::CAN_BE_BASE_IN_ATOMIC_SWAP))
+    {
+        innerResult().code(
+                CreateASwapBidCreationRequestResultCode::BASE_ASSET_CANNOT_BE_SWAPPED);
+        return false;
+    }
+
+    if (!areQuoteAssetsValid(db, baseAssetFrame->getCode()))
+    {
+        return false;
+    }
+
+    if (!baseBalanceFrame->hasEnoughAmount(aSwapBidCreationRequest.amount))
+    {
+        innerResult().code(
+                CreateASwapBidCreationRequestResultCode::BASE_BALANCE_UNDERFUNDED);
+        return false;
+    }
+
+    auto requestFrame = ReviewableRequestFrame::createNew(0, getSourceID(),
+                                                          app.getMasterID(), nullptr,
+                                                          ledgerManager.getCloseTime());
+    auto& requestEntry = requestFrame->getRequestEntry();
+    requestEntry.body.type(ReviewableRequestType::CREATE_ATOMIC_SWAP_BID);
+    requestEntry.body.aSwapBidCreationRequest() = aSwapBidCreationRequest;
+
+    requestFrame->recalculateHashRejectReason();
+
+    ReviewableRequestHelper::Instance()->storeAdd(delta, db, requestFrame->mEntry);
+
+    innerResult().code(CreateASwapBidCreationRequestResultCode::SUCCESS);
+    innerResult().success().requestID = requestFrame->getRequestID();
+
+    return true;
+}
+
+bool CreateASwapBidCreationRequestOpFrame::doCheckValid(Application &app)
+{
+    auto& const aSwapCreationRequest = mCreateASwapBidCreationRequest.request;
+
+    if (aSwapCreationRequest.amount == 0)
+    {
+        innerResult().code(CreateASwapBidCreationRequestResultCode::INVALID_AMOUNT);
+        return false;
+    }
+
+    if (!isValidJson(aSwapCreationRequest.details))
+    {
+        innerResult().code(CreateASwapBidCreationRequestResultCode::INVALID_DETAILS);
+        return false;
+    }
+
+    set<AssetCode> quoteAssets;
+    for (auto const& quoteAsset : aSwapCreationRequest.quoteAssets)
+    {
+        if (!AssetFrame::isAssetCodeValid(quoteAsset.quoteAsset))
+        {
+            innerResult().code(
+                    CreateASwapBidCreationRequestResultCode::INVALID_QUOTE_ASSET);
+            return false;
+        }
+
+        if (quoteAssets.find(quoteAsset.quoteAsset) != quoteAssets.end())
+        {
+            innerResult().code(
+                    CreateASwapBidCreationRequestResultCode::INVALID_QUOTE_ASSET);
+            return false;
+        }
+
+        if (quoteAsset.price == 0)
+        {
+            innerResult().code(CreateASwapBidCreationRequestResultCode::INVALID_PRICE);
+        }
+
+        const bool isQuoteAmountFits = OfferManager::calculateQuoteAmount(
+                aSwapCreationRequest.amount, quoteAsset.price) > 0;
+        if (!isQuoteAmountFits)
+        {
+            innerResult().code(
+                    CreateASwapBidCreationRequestResultCode::ATOMIC_SWAP_BID_OVERFLOW);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+}
---
+++ src/transactions/CreateASwapBidCreationRequestOpFrame.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "OperationFrame.h"
+
+namespace stellar
+{
+
+class CreateASwapBidCreationRequestOpFrame : public OperationFrame
+{
+protected:
+    CreateASwapBidCreationRequestOp& const mCreateASwapBidCreationRequest;
+
+    CreateASwapBidCreationRequestResult& innerResult()
+    {
+        return mResult.tr().createASwapBidCreationRequestResult();
+    }
+
+    std::unordered_map<AccountID, CounterpartyDetails>
+    getCounterpartyDetails(Database &db, LedgerDelta *delta) const override;
+
+    SourceDetails
+    getSourceAccountDetails(std::unordered_map<AccountID, CounterpartyDetails>
+                            counterpartiesDetails, int32_t ledgerVersion) const override;
+
+public:
+    CreateASwapBidCreationRequestOpFrame(Operation const &op, OperationResult &opRes,
+                                      TransactionFrame &parentTx);
+
+    bool isQuoteAssetValid(Database& db, AssetCode baseAssetCode,
+                           AssetCode quoteAssetCode);
+
+    bool areQuoteAssetsValid(Database& db, AssetCode baseAssetCode);
+
+    bool doApply(Application &app, LedgerDelta &delta,
+                 LedgerManager &ledgerManager) override;
+
+    bool doCheckValid(Application &app) override;
+
+    static CreateASwapBidCreationRequestResultCode getInnerCode(OperationResult& res)
+    {
+        return res.tr().createASwapBidCreationRequestResult().code();
+    }
+
+    std::string getInnerResultCodeAsStr() override
+    {
+        return xdr::xdr_traits<CreateASwapBidCreationRequestResultCode>::enum_name(
+                innerResult().code());
+    }
+};
+
+}
