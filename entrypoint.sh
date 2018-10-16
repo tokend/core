0fa56ee95c084e65bda1b7e99e6b84a64cd57c14
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-10-16 20:56:26 +0300

    Fix postgres healtcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eax
+set -e
 
 CONFIG=$1
 BIN=/usr/local/bin/core
--- pg-entrypoint.sh
+++ pg-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/dumb-init /bin/bash
-set -eax
+set -e
 
 pg-entrypoint.sh postgres &
 
-while ! pg_isready -U$POSTGRES_USER > /dev/null 2> /dev/null; do
+while ! pg_isready -U$POSTGRES_USER -hlocalhost > /dev/null 2> /dev/null; do
     echo "postgres not ready yet"
     sleep 3
 done
+
 core-entrypoint.sh $@
