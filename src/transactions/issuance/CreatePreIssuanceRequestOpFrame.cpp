159b035d20dd54000d11ac989ee9e5bad4936a60
Yaroslav Panasenko <yar.panasenko@gmail.com>
2019-05-17 15:23:18 +0300

    Refactored checking account rules in CreatePreIssuanceRequestOp

--- src/transactions/issuance/CreatePreIssuanceRequestOpFrame.cpp
+++ src/transactions/issuance/CreatePreIssuanceRequestOpFrame.cpp
@@ -134,19 +134,21 @@ CreatePreIssuanceRequestOpFrame::tryGetOperationConditions(StorageHelper& storag
 							  			std::vector<OperationCondition>& result,
 							  			LedgerManager& ledgerManager) const
 {
-    if (ledgerManager.shouldUse(LedgerVersion::FIX_NOT_CHECKING_SET_TASKS_PERMISSIONS))
+    if (!ledgerManager.shouldUse(LedgerVersion::FIX_NOT_CHECKING_SET_TASKS_PERMISSIONS))
     {
-        AccountRuleResource resource(LedgerEntryType::REVIEWABLE_REQUEST);
-        resource.reviewableRequest().details.requestType(ReviewableRequestType::CREATE_PRE_ISSUANCE);
+        return true;
+    }
 
-        if (mCreatePreIssuanceRequest.allTasks)
-        {
-            result.emplace_back(resource, AccountRuleAction::CREATE_WITH_TASKS, mSourceAccount);
-        }
-        else
-        {
-            result.emplace_back(resource, AccountRuleAction::CREATE, mSourceAccount);
-        }
+    AccountRuleResource resource(LedgerEntryType::REVIEWABLE_REQUEST);
+    resource.reviewableRequest().details.requestType(ReviewableRequestType::CREATE_PRE_ISSUANCE);
+
+    if (mCreatePreIssuanceRequest.allTasks)
+    {
+        result.emplace_back(resource, AccountRuleAction::CREATE_WITH_TASKS, mSourceAccount);
+    }
+    else
+    {
+        result.emplace_back(resource, AccountRuleAction::CREATE, mSourceAccount);
     }
 
 	// only asset pre issuer can do pre issuance;
